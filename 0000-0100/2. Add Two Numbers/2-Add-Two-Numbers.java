1a164eb4646c198d06ef95a580355eace8cf2fde
ivanushka05 <akkaunt9043@gmail.com>
2024-06-17 01:51:04 +0300

    fix 2 number

--- 0000-0100/2. Add Two Numbers/2-Add-Two-Numbers.java
+++ 0000-0100/2. Add Two Numbers/2-Add-Two-Numbers.java
@@ -11,32 +11,31 @@
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode listNode1 = l1;
-        ListNode listNode2 = l2;
+
         ListNode sum = new ListNode(0);
         ListNode res = sum;
 
 
-        while (listNode1 != null && listNode2 != null) {
+        while (l1 != null && l2 != null) {
 
-            sum.val += listNode1.val + listNode2.val;
+            sum.val += l1.val + l2.val;
             if(sum.val > 9){
                 sum.val -= 10;
                 sum.next = new ListNode(1);
             }else{
-                if(listNode1.next != null || listNode2.next != null)
+                if(l1.next != null || l2.next != null)
                     sum.next = new ListNode(0);
 
             }
-            if(listNode1.next == null && listNode2.next != null){
-                listNode1.next = new ListNode(0);
+            if(l1.next == null && l2.next != null){
+                l1.next = new ListNode(0);
             }
-            if(listNode1.next != null && listNode2.next == null){
-                listNode2.next = new ListNode(0);
+            if(l1.next != null && l2.next == null){
+                l2.next = new ListNode(0);
             }
             sum = sum.next;
-            listNode1 = listNode1.next;
-            listNode2 = listNode2.next;
+            l1 = l1.next;
+            l2 = l2.next;
         }
 
 
